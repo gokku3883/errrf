b21627d2779fe0450eeeab458bd5aed47533b17a
gokku3883 <sharmarishabh3325@gmail.com>
2025-01-04 10:52:40 +0530

    root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,36 @@
-t
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "bugscanner-go",
+	Short: "This tool is dedicated to DARKTUNNEL.NET, please support us if you find this tool useful.",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bugscanner-go.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
