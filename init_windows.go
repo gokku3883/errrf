ed76389c1566ef784ec843eeb7756ef8cda95b58
gokku3883 <sharmarishabh3325@gmail.com>
2025-01-04 11:34:13 +0530

    init_windows.go

--- init_windows.go
+++ init_windows.go
@@ -1,1 +1,16 @@
-y
+package main
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+func init() {
+	stdout := windows.Handle(os.Stdout.Fd())
+
+	var originalMode uint32
+
+	windows.GetConsoleMode(stdout, &originalMode)
+	windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
