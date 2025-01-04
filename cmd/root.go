e4db18b4e1e31047e2b70df1d08be5ce310e7f30
gokku3883 <sharmarishabh3325@gmail.com>
2025-01-04 10:55:47 +0530

    root.go

--- cmd/root.go
+++ cmd/root.go
@@ -34,3 +34,38 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+var (
+	colorD1 = color.New()
+	colorB1 = color.New(color.FgHiBlack)
+	colorW1 = color.New(color.FgWhite, color.Bold)
+	colorG1 = color.New(color.FgGreen, color.Bold)
+	colorC1 = color.New(color.FgCyan, color.Bold)
+	colorY1 = color.New(color.FgYellow, color.Bold)
+)
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".bugscanner-go" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".bugscanner-go")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
