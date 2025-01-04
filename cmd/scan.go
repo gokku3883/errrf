df34b8571e0b8d7debd79d9796734de746d008ff
gokku3883 <sharmarishabh3325@gmail.com>
2025-01-04 10:58:56 +0530

    scan.go

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,1 +1,18 @@
-y
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "Subcommand for scanning",
+}
+
+var scanFlagThreads int
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+
+	scanCmd.PersistentFlags().IntVarP(&scanFlagThreads, "threads", "t", 64, "total threads to use")
+}
